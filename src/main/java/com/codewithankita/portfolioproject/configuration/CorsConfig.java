d22a9ea5316fe7718adb5973f6878d645f7a7fcd
Ankita <ankitathakurmfp@gmail.com>
2026-03-22 12:59:44 +0900

    Update allowed origins in CORS configuration

--- src/main/java/com/codewithankita/portfolioproject/configuration/CorsConfig.java
+++ src/main/java/com/codewithankita/portfolioproject/configuration/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("https://ankita-thakur-28.github.io")
                         .allowedMethods("GET","POST","PUT","DELETE");
 
             }

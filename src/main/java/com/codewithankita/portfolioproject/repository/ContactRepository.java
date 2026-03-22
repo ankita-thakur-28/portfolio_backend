f7faeaff0ee07f65e0e96860dd0e5180dbad6910
Ankita <ankitathakurmfp@gmail.com>
2026-03-23 00:12:50 +0900

    Update ContactRepository to use external DB config
    Refactor database connection to use properties for username, password, and URL.

--- src/main/java/com/codewithankita/portfolioproject/repository/ContactRepository.java
+++ src/main/java/com/codewithankita/portfolioproject/repository/ContactRepository.java
@@ -1,15 +1,26 @@
 package com.codewithankita.portfolioproject.repository;
 import com.codewithankita.portfolioproject.model.Contact;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import java.sql.*;
 
 @Repository
 public class ContactRepository {
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.url}")
+    private String url;
+
     public void addContact(Contact contact){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/portfolio","root","Ankita@1928");
+            Connection con =DriverManager.getConnection(url, username, password);
 
             String query = "INSERT INTO contact(name, email, message) VALUES(?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(query);

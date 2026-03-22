7b6b5c9fe16110bea9d80fef264ac2793b886b95
Ankita <ankitathakurmfp@gmail.com>
2026-03-23 00:21:15 +0900

    Update CORS configuration to allow headers and disable credentials
    Added allowed headers and disabled credentials in CORS configuration.

--- src/main/java/com/codewithankita/portfolioproject/configuration/CorsConfig.java
+++ src/main/java/com/codewithankita/portfolioproject/configuration/CorsConfig.java
@@ -17,7 +17,9 @@ public class CorsConfig {
 
                 registry.addMapping("/**")
                         .allowedOrigins("https://ankita-thakur-28.github.io")
-                        .allowedMethods("GET","POST","PUT","DELETE");
+                        .allowedMethods("GET","POST","PUT","DELETE").
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
 
             }
         };
